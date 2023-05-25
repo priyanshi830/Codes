7d262f667e9423d3528bc834a2c0fe230c2617ac
priyanshi830 <priyanshi1077.be21@chitkara.edu.in>
2023-05-25 19:37:52 +0530

    Create if-else (Decision Making).java

---
+++ Geeks for Geeks/if-else (Decision Making).java
@@ -0,0 +1,26 @@
+https://practice.geeksforgeeks.org/problems/java-if-else-decision-making0924/0
+
+
+
+
+
+
+//User function Template for Java
+class Solution{
+    static String compareNM(int n,int m){
+       // Scanner sc= new Scanner(System.in);
+        // System.out.println("n = ");
+        // n=sc.nextInt();
+        // System.out.println("m = ");
+        // m=sc.nextInt();
+       if(n<m)
+           return ("lesser");
+      
+       else if(n>m)
+           return ("greater");
+       
+       else
+           return ("equal");
+       }
+   
+    }
